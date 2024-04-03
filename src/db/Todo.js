22f76f97bdad6c6567141ade84a47a59d1b048f3
MirzayevOrzu <mirzayevorzu127@gmail.com>
2024-04-04 03:05:41 +0500

    no need to many-to-many

--- src/db/Todo.js
+++ src/db/Todo.js
@@ -23,9 +23,4 @@ User.hasMany(Todo, { foreignKey: "userId", as: "user" });
 Todo.belongsTo(Guide, { foreignKey: "guideId", as: "guide" });
 Guide.hasMany(Todo, { foreignKey: "guideId", as: "guide" });
 
-// Guide.belongsToMany(User, { through: Todo, onDelete: "CASCADE" });
-// User.belongsToMany(Guide, { through: Todo, onDelete: "CASCADE" });
-// User.hasMany(Todo, { foreignKey: "userId" });
-// Guide.hasMany(Todo, { foreignKey: "guideId" });
-
 module.exports = Todo;
